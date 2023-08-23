e293fbe21e49c624ddde1df8ac3f5e5927d4dd75
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-08-24 02:51:35 +0545

    updated config file

--- pm2run.config.js
+++ pm2run.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "ryuuzu-portfolio-nextjs-5000",
-            cwd: "/root/projects/portfolio-nextjs",
+            cwd: "/root/projects/ryuuzu.xyz",
             script: "npm",
             args: "start",
             env: {
