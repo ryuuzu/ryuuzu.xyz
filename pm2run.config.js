7b2e52cb63b3eb57d47dadbbcdd87dc72e2fb12b
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-08-24 02:44:40 +0545

    added pm2 file for hosting

---
+++ pm2run.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: "ryuuzu-portfolio-nextjs-5000",
+            cwd: "/root/projects/portfolio-nextjs",
+            script: "npm",
+            args: "start",
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
+};
