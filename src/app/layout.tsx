e2ce3a631d245f4cc371d3487aee5acc4d92447e
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-08-31 12:51:32 +0545

    installed the clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+import { useEffect } from "react";
 
 import "./globals.css";
 
@@ -21,7 +23,21 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={`${dmSans.className}`}>{children}</body>
+            <body className={`${dmSans.className}`}>
+                <Script
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "impchiwk8z");`,
+                    }}
+                    id={"ms-clarity"}
+                />
+                {children}
+            </body>
         </html>
     );
 }
